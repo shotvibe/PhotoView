3cf27dd3ea72d0bc3098bf2e3509b68e6233910f
Alexander Belonogov <sashabelonogov@gmail.com>
2013-03-19 16:39:38 +0700

    Removed unused code

--- library/src/uk/co/senab/photoview/PhotoViewAttacher.java
+++ library/src/uk/co/senab/photoview/PhotoViewAttacher.java
@@ -279,26 +279,6 @@ public class PhotoViewAttacher implements IPhotoView, View.OnTouchListener, Vers
 		if (null != imageView && hasDrawable(imageView)) {
 			mSuppMatrix.postTranslate(dx, dy);
 			checkAndDisplayMatrix();
-			
-			/**
-			 * Here we decide whether to let the ImageView's parent to start
-			 * taking over the touch event.
-			 *
-			 * First we check whether this function is enabled. We never want the
-             * parent to take over if we're scaling. We then check the edge we're
-             * on, and the direction of the scroll (i.e. if we're pulling against
-             * the edge, aka 'overscrolling', let the parent take over).
-			 */
-			
-			//Improved by me
-			RectF rect = getDisplayRect();
-			
-			if (mAllowParentInterceptOnEdge && !mScaleDragDetector.isScaling()) {
-				if (mScrollEdge == EDGE_BOTH || (mScrollEdge == EDGE_LEFT && dx >= 1f && (int)rect.left==0)
-					|| (mScrollEdge == EDGE_RIGHT && dx <= -1f && (int)rect.right==((Activity) getImageView().getContext()).getWindowManager().getDefaultDisplay().getWidth())) {
-					imageView.getParent().requestDisallowInterceptTouchEvent(false);
-				}
-			}
 		}
 	}
 
